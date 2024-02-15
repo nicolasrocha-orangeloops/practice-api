9a875e9896252814fea8741ced344c4f35be2af4
nicolasrocha-orangeloops <nicolas.rocha@orangeloops.com>
2024-02-15 16:56:17 -0300

    Deleted unused imports

--- src/main/java/com/practice/practiceapi/core/URLMap.java
+++ src/main/java/com/practice/practiceapi/core/URLMap.java
@@ -1,9 +1,6 @@
 package com.practice.practiceapi.core;
 
-import com.practice.practiceapi.controllers.URLApiController;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
